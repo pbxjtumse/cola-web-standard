d2a68bdd0ec72fb390a2a2ad5622b6e76c8f60df
pbxjtumse <pbxjtumse@outlook.com>
2026-08-24 22:44:46 +0800

    技术组件-分布式锁组件-抽象spi 层

--- component/distributed-lock-component/distributed-lock-core/src/test/java/com/xjtu/iron/distributed/lock/core/spi/protocol/LockAcquireRequestTest.java
+++ component/distributed-lock-component/distributed-lock-core/src/test/java/com/xjtu/iron/distributed/lock/core/spi/protocol/LockAcquireRequestTest.java
@@ -1,6 +1,7 @@
 package com.xjtu.iron.distributed.lock.core.spi.protocol;
 
 import com.xjtu.iron.distributed.lock.api.model.LockOptions;
+import com.xjtu.iron.distributed.lock.spi.protocol.LockAcquireRequest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
